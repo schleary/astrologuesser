0497b3c086f752833efd51cf1862d1fe7fb5f361
Holly Leary <schleary@gmail.com>
2015-01-07 17:01:00 -0800

    dictionary mapping index

--- astrologuesser/urls.py
+++ astrologuesser/urls.py
@@ -4,4 +4,10 @@ from astrologuesser import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+     # ex: /astrologuesser/5/
+    url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+    # ex: /astrologuesser/5/results/
+    url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
+    # ex: /astrologuesser/5/vote/
+    url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
 )
